5a5617b1f7200f987eae921b5b7346335c6d3d6d
Joseph <josephwaizy@gmail.com>
2026-07-31 19:50:29 +0800

    Basic player movements and really basic time rewinding

---
+++ Scripts/Player/player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+
+@export var SPEED = 300.0
+@export var JUMP_VELOCITY = -400.0
+@export var shadow: PackedScene
+var lastPoint: Vector2
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	if Input.is_action_just_pressed("Rewind"):
+		global_position = lastPoint
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	move_and_slide()
+	if Input.is_action_just_pressed("AddPoint"):
+		lastPoint = global_position
+	shadow.global_position = lastPoint
