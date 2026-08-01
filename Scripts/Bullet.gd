989128e4d28bbbb3b85b40fd7034a18dbbe4e19f
Aaron Mathew <aaron.nadar@hotmail.com>
2026-08-02 00:23:47 +0800

    Push needed!

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -4,8 +4,8 @@ var SPEED = 100
 var distance: float = 0.0
 
 func _process(delta: float):
-	position.x = SPEED
-	distance += SPEED
+	position.x += SPEED * delta
+	distance += SPEED * delta
 	
 	if distance >= 670:
 		queue_free() 
--- Scripts/Turret.gd
+++ Scripts/Turret.gd
@@ -1,26 +1,33 @@
 extends StaticBody2D
 
 var Target: Node2D = null
+var canShoot: bool = false
 
 func _process(delta: float):
 	if Target != null:
 		var directionAngle = global_position.angle_to_point(Target.global_position) + PI
 		$Top.rotation = lerp_angle(rotation, directionAngle, 1)
 		Shoot()
-		$Timer.start()
 
 func Shoot():
-	const Bullet = preload("res://Scenes/Bullet.tscn")
-	var newBullet = Bullet.instantiate()
-	$Top.add_child(newBullet)
-	
-	newBullet.global_transform = $Top.global_transform
+	if canShoot:
+		const Bullet = preload("res://Scenes/Bullet.tscn")
+		var newBullet = Bullet.instantiate()
+		$Top.add_child(newBullet)
+		
+		newBullet.global_transform = $Top.global_transform
+		canShoot = false
+		print("Shoot")
+		$Timer.start()
 
 func _on_detection_body_entered(body: Node2D):
-	Target = body
+	print(body)
+	if body.name == "Player":
+		Target = body
 
 func _on_detection_body_exited(body: Node2D):
-	Target = null
+	if body.name == "Player":
+		Target = null
 
 func _on_timer_timeout():
-	pass
+	canShoot = true
